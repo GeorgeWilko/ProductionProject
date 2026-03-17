e91774218e110ade02d36a2c58d6dfed602630a4
Student <g.wilkinson6868@student.leedsbeckett.ac.uk>
2026-03-17 19:29:09 +0000

    New URLS added for the categories to show on the booking page.

--- mysite/website/urls.py
+++ mysite/website/urls.py
@@ -10,6 +10,26 @@ urlpatterns = [
          views.booking,
          name='booking'
          ),
+    path('equipment/peripherals/',
+         views.peripherals,
+         name='peripherals'
+         ),
+    path('equipment/computing/',
+         views.computing,
+         name='computing'
+         ),
+    path('equipment/display/',
+         views.display,
+         name='display'
+         ),
+    path('equipment/storage/',
+         views.storage,
+         name='storage'
+         ),
+    path('equipment/accessories/',
+         views.accessories,
+         name='accessories'
+         ),
     path('confirmation/',
          views.confirmation,
          name='confirmation'
