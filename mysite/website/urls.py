dcc7204a6665fb734f6811875a1e81647440e435
Student <g.wilkinson6868@student.leedsbeckett.ac.uk>
2026-03-18 17:10:36 +0000

    Added the media_url line as i struggled to get my images to load and the assistance was this line was missing

--- mysite/website/urls.py
+++ mysite/website/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -34,4 +36,4 @@ urlpatterns = [
          views.confirmation,
          name='confirmation'
          ),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
